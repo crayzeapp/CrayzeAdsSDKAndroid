996af5a597afff27b7a8c19b3b38090d2c85a763
Amitay Molko <amitaymolko@gmail.com>
2016-08-25 10:01:49 +0300

    working demo

--- app/src/main/java/com/crayze/crayzeadssdkandroid/MainActivity.java
+++ app/src/main/java/com/crayze/crayzeadssdkandroid/MainActivity.java
@@ -3,11 +3,18 @@ package com.crayze.crayzeadssdkandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crayze.crayzeadssdk.Crayze;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Crayze.getInstance().setAppToken("578a7d1470ea9");
+        //        Crayze.getInstance().setTestMode(true);
+
+        Crayze.getInstance().startApp(this);
     }
 }
